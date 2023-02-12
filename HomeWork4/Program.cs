f90a2ed9245b3f97c538073ad1522c31843a13eb
Amirbek Shipulin <amirbek.shipulin@mail.ru>
2023-02-12 16:37:49 +0400

    HomeWork4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+
+while(temp < N)
+{
+    temp = temp + 1;
+    if(temp % 2 == 0)
+    Console.Write($"{temp} ");
+}
+
+
+
+
+
+
